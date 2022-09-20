a240b85c4865976716b00f25b52c6fc4aeee638c
Adam McDaniel <adam.mcdaniel17@gmail.com>
2022-09-19 20:03:03 -0400

    Removed gitignore

---
+++ docs/acid/web-impl/acid_web_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main_js(): void;
+export function compile_and_run(a: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_start(): void;
