0d0aaf11af9c9b749ca789fe3fe04b10dbea34e7
Adam McDaniel <adam.mcdaniel17@gmail.com>
2023-04-17 11:49:01 -0400

    Added links to table in docs

--- docs/sage/web/web.js
+++ docs/sage/web/web.js
@@ -248,6 +248,7 @@ function getImports() {
     imports.wbg.__wbg_log_e0f22e0e407ab7fc = function(arg0, arg1) {
         console.log(getStringFromWasm0(arg0, arg1));
     };
+<<<<<<< HEAD
     imports.wbg.__wbg_instanceof_HtmlTextAreaElement_4bc39f9d861a6832 = function(arg0) {
         let result;
         try {
@@ -282,6 +283,8 @@ function getImports() {
         const ret = getObject(arg0).document;
         return isLikeNone(ret) ? 0 : addHeapObject(ret);
     };
+=======
+>>>>>>> da6f5d3 (Added links to table in docs)
     imports.wbg.__wbg_getElementById_eb93a47327bb5585 = function(arg0, arg1, arg2) {
         const ret = getObject(arg0).getElementById(getStringFromWasm0(arg1, arg2));
         return isLikeNone(ret) ? 0 : addHeapObject(ret);
@@ -303,6 +306,43 @@ function getImports() {
         getInt32Memory0()[arg0 / 4 + 1] = len0;
         getInt32Memory0()[arg0 / 4 + 0] = ptr0;
     };
+<<<<<<< HEAD
+=======
+    imports.wbg.__wbg_instanceof_Window_e266f02eee43b570 = function(arg0) {
+        let result;
+        try {
+            result = getObject(arg0) instanceof Window;
+        } catch {
+            result = false;
+        }
+        const ret = result;
+        return ret;
+    };
+    imports.wbg.__wbg_document_950215a728589a2d = function(arg0) {
+        const ret = getObject(arg0).document;
+        return isLikeNone(ret) ? 0 : addHeapObject(ret);
+    };
+    imports.wbg.__wbg_instanceof_HtmlTextAreaElement_4bc39f9d861a6832 = function(arg0) {
+        let result;
+        try {
+            result = getObject(arg0) instanceof HTMLTextAreaElement;
+        } catch {
+            result = false;
+        }
+        const ret = result;
+        return ret;
+    };
+    imports.wbg.__wbg_value_00fb0fdc46959169 = function(arg0, arg1) {
+        const ret = getObject(arg1).value;
+        const ptr0 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        const len0 = WASM_VECTOR_LEN;
+        getInt32Memory0()[arg0 / 4 + 1] = len0;
+        getInt32Memory0()[arg0 / 4 + 0] = ptr0;
+    };
+    imports.wbg.__wbg_setvalue_f92ff20dd33356a8 = function(arg0, arg1, arg2) {
+        getObject(arg0).value = getStringFromWasm0(arg1, arg2);
+    };
+>>>>>>> da6f5d3 (Added links to table in docs)
     imports.wbg.__wbg_newnoargs_2b8b6bd7753c76ba = function(arg0, arg1) {
         const ret = new Function(getStringFromWasm0(arg0, arg1));
         return addHeapObject(ret);
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,4 +1,8 @@
 var sourcesIndex = JSON.parse('{\
+<<<<<<< HEAD
 "sage":["",[["asm",[],["core.rs","location.rs","mod.rs","std.rs"]],["lir",[["expr",[["ops",[],["arithmetic.rs","assign.rs","comparison.rs","logic.rs","mod.rs","unary.rs"]]],["builtin.rs","const_expr.rs","expression.rs","mod.rs","pattern.rs","procedure.rs"]],["types",[],["check.rs","inference.rs","mod.rs","size.rs"]]],["compile.rs","env.rs","mod.rs"]],["targets",[],["c.rs","mod.rs"]],["vm",[["interpreter",[],["core.rs","mod.rs","std.rs"]]],["core.rs","mod.rs","std.rs"]]],["io.rs","lib.rs","parse.rs"]]\
+=======
+"sage":["",[["asm",[],["core.rs","location.rs","mod.rs","std.rs"]],["lir",[["expr",[["ops",[],["arithmetic.rs","assign.rs","comparison.rs","logic.rs","mod.rs","unary.rs"]]],["builtin.rs","const_expr.rs","expression.rs","mod.rs","procedure.rs"]],["types",[],["check.rs","inference.rs","mod.rs","size.rs"]]],["compile.rs","env.rs","mod.rs"]],["targets",[],["c.rs","mod.rs"]],["vm",[["interpreter",[],["core.rs","mod.rs","std.rs"]]],["core.rs","mod.rs","std.rs"]]],["io.rs","lib.rs","parse.rs"]]\
+>>>>>>> da6f5d3 (Added links to table in docs)
 }');
 createSourceSidebar();
