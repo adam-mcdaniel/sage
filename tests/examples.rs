4f666396540812f1d29c9be6c3b93faab581deb8
Adam McDaniel <adam.mcdaniel17@gmail.com>
2024-09-03 16:07:35 -0400

    Fixed output of example due to interpreter tape size change

--- tests/examples.rs
+++ tests/examples.rs
@@ -145,10 +145,30 @@ fn test_frontend_examples_helper() {
 
 #[test]
 fn test_lir_examples() {
+    let mut builder = env_logger::Builder::from_default_env();
+    builder.format_timestamp(None);
+    builder.filter(
+        None,
+        // LogLevel::Error if args.debug.is_none() => log::LevelFilter::Error,
+        // LogLevel::Warn if args.debug.is_none() => log::LevelFilter::Warn,
+        // LogLevel::Off if args.debug.is_none() => log::LevelFilter::Error,
+        // LogLevel::Info if args.debug.is_none() => log::LevelFilter::Info,
+        // LogLevel::Trace => log::LevelFilter::Trace,
+        log::LevelFilter::Info,
+    );
+    builder.init();
+
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(1)
+        .stack_size(512 * 1024 * 1024)
+        .build_global()
+        .unwrap();
+    
     // Compiling most examples overflows the tiny stack for tests.
     // So, we spawn a new thread with a larger stack size.
     let child = std::thread::Builder::new()
-        .stack_size(64 * 1024 * 1024)
+        .stack_size(512 * 1024 * 1024)
         .spawn(test_lir_examples_helper)
         .unwrap();
 
