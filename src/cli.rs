56c0342f455e5a3956d48228c356b02ccb631089
Adam McDaniel <adam.mcdaniel17@gmail.com>
2024-09-04 17:05:30 -0400

    Reverted larger stack size back to normal

--- src/cli.rs
+++ src/cli.rs
@@ -19,8 +19,8 @@ use std::{
 use log::error;
 
 // The stack sizes of the threads used to compile the code.
-// const RELEASE_STACK_SIZE_MB: usize = 512;
-const RELEASE_STACK_SIZE_MB: usize = 2048 * 10;
+const RELEASE_STACK_SIZE_MB: usize = 512;
+// const RELEASE_STACK_SIZE_MB: usize = 2048;
 const DEBUG_STACK_SIZE_MB: usize = RELEASE_STACK_SIZE_MB;
 
 #[derive(clap::ValueEnum, Default, Clone, Debug, PartialEq)]
